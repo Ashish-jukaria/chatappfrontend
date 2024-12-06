36ec9469f241cecd8128db5d03baddfe041dc362
Ashish-jukaria <ashishjukaria2001@gmail.com>
2024-12-06 09:15:02 +0530

    deply again

--- src/Components/Chats.tsx
+++ src/Components/Chats.tsx
@@ -6,6 +6,29 @@ import { baseurl } from "./Context"
 export const Chats=({roomId,username}:any)=>{
     const message=useRef<HTMLInputElement|null>(null)
     const ws=useSocket()
+    async function handleKey(event:any){
+        if(event.key=='Enter'){
+        if(ws && message.current){
+            ws.send(JSON.stringify({
+                type:"chat",
+                payload:{
+                    "roomId":roomId,
+                    "user":username,
+                    "message":`${message.current.value}`
+                }
+            }))
+        }
+        if (message.current){
+        await axios.post(`${baseurl}/message`,{"roomId":roomId,
+            "message":message.current?.value
+        },{
+            headers:{
+                "token":localStorage.getItem("token")
+            }
+        })
+
+    }}
+    }
     async function handleClick(){
         if(ws && message.current){
             ws.send(JSON.stringify({
@@ -31,7 +54,7 @@ export const Chats=({roomId,username}:any)=>{
     return(
         <>
         <div className="flex w-screen items-center justify-center">
-        <input className="border-black border-2 m-2 w-1/4 p-2 " type="text" placeholder="message" ref={message} />
+        <input className="border-black border-2 m-2 w-1/4 p-2 " type="text" onKeyDown={handleKey} placeholder="message" ref={message} />
         <button className="bg-blue-700 py-2 px-4 hover:bg-blue-600" onClick={handleClick}>Send</button>
         </div>
 
