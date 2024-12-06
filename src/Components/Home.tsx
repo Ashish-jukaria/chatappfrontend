aa1f2483eb263358cbcbab2f68315e26d318fed4
Ashish-jukaria <ashishjukaria2001@gmail.com>
2024-12-06 10:36:39 +0530

    text -size

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,11 +1,11 @@
-export const Home =()=>{
+export const Home = () => {
     return (
-        <>
-        <div className="flex flex-col justify-center items-center text-center my-20  font-mono text-8xl ">
-            <div>Connect instantly, chat effortlessly – your conversations, simplified."</div>
-
+      <>
+        <div className="flex flex-col justify-center items-center text-center my-20 font-mono 
+                        lg:text-8xl md:text-6xl sm:text-4xl text-3xl">
+          <div>Connect instantly, chat effortlessly – your conversations, simplified.</div>
         </div>
-
-        </>
-    )
-}
+      </>
+    );
+  };
+  
